284429c6ac2a91b21d993cf63494b4b5ae287331
Diego Rangel Piranga Costa <diegorang@outlook.com.br>
2021-05-17 14:05:34 -0300

    Update CustomHash.cpp

--- Data Structures/CustomHash.cpp
+++ Data Structures/CustomHash.cpp
@@ -3,7 +3,7 @@
 struct custom{
     static uint64_t splitmix64(uint64_t x) {
     	x += 0x9e3779b97f4a7c15;
-      x = (x^(x>>30))*0xbf58476d1ce4e5b9;
+        x = (x^(x>>30))*0xbf58476d1ce4e5b9;
     	x = (x^(x>>27))*0x94d049bb133111eb;
       return x^(x>>31);
     }
